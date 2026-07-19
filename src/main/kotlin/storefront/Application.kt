0a1ab244455936ca9b9f4e04f877def8e6a5aaba
Hayden <haydeneastwell@proton.me>
2026-07-19 23:44:11 +0100

    maintenance: remove redundant `embeddedServer` declaration

--- src/main/kotlin/storefront/Application.kt
+++ src/main/kotlin/storefront/Application.kt
@@ -3,8 +3,6 @@ package haydende.storefront
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import kotlinx.serialization.json.Json
 
@@ -15,11 +13,5 @@ fun Application.main() {
                 ignoreUnknownKeys = true
             }
         )
-    }
-
-    embeddedServer(Netty, port = 8080) {
-        install(ContentNegotiation) {
-            json()
-        }
     }
 }
