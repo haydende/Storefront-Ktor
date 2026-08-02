afcf0e937169b0127065ba2fa3946146fe0d6ade
Hayden <haydeneastwell@proton.me>
2026-08-02 21:35:48 +0100

    Require id in UpdateUserDTO

--- src/main/kotlin/storefront/model/dto/UpdateUserDTO.kt
+++ src/main/kotlin/storefront/model/dto/UpdateUserDTO.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.*
 
 @Serializable
 data class UpdateUserDTO(
-    val id: Int? = null,
+    val id: Int,
     val firstName: String? = null,
     val lastName: String? = null,
     val email: String? = null,
